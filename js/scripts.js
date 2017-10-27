6569ddf7e9074d6fc4ab5e64f2aaabd6459cd129
Adam Smith <alspdx@gmail.com>
2017-10-27 09:40:58 -0700

    add ux logic to take in user input and output final message in scripts file

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,7 @@ $(function() {
   $("#user-input").submit(function(event) {
     event.preventDefault();
     var numberInput = parseInt($("#number-input").val());
-    var outputMessage = hal9000(numberInput)
+    var outputMessage = hal9000(numberInput);
+    $("#output-message").text(outputMessage);
   });
 });
