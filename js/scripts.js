096990c0c9ca361988f9192603c4caf32f64375a
Adam Smith <alspdx@gmail.com>
2017-10-27 11:33:48 -0700

    add error message if number is divisible by '3' but not '0'

--- js/scripts.js
+++ js/scripts.js
@@ -3,13 +3,16 @@ var zeroRegex = /[0]/;
     zeroOneRegex = /[01]/;
     numRegex = /^[0-9]*$/;
     errorNaN = "I think you know what the problem is just as well as I do. This mission is too important for me to allow you to jeopardize it. Please enter a numerical value, Dave."
+    errorMultipleOfThree = "I'm sorry, Dave. I'm afraid I can't do that."
 
-var beepBoop = function(number) {
-  if (!zeroOneRegex.test(number)) {
+var numberEvaluator = function(number) {
+  if ((parseInt(number) % 3 === 0) && (parseInt(number) !== 0)) {
+    return errorMultipleOfThree
+  } else if (!zeroOneRegex.test(number)) {
     return number;
   } else if (!zeroRegex.test(number)) {
     return "Boop"
-  } else if (!zeroRegex.test(number)){
+  } else if (!oneRegex.test(number)){
     return "Beep";
   } else {
     return "Beep-Boop"
@@ -18,7 +21,7 @@ var beepBoop = function(number) {
 
 var outputStringify = function(numArray) {
   var outputString = numArray.map(function(number) {
-    return beepBoop(number);
+    return numberEvaluator(number);
   });
   return outputString.join(", ");
 };
