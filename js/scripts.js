4b2aee516d0df10ada2753cbffe04541a03980a0
Adam Smith <alspdx@gmail.com>
2017-10-27 10:57:41 -0700

    add function to replace array items containing '0' with 'beep '

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,38 @@
-var numTest = /^[0-9]*$/
+var zeroRegex = /[0]/
+var numRegex = /^[0-9]*$/
 var errorNaN = "I think you know what the problem is just as well as I do. This mission is too important for me to allow you to jeopardize it. Please enter a numerical value, Dave."
+
+var beepZeros = function(number) {
+  if (!zeroRegex.test(number)) {
+    return number;
+  } else {
+    return "Beep";
+  };
+};
+
+var outputStringify = function(numArray) {
+  var outputString = numArray.map(function(number) {
+    return beepZeros(number);
+  });
+  return outputString.join(", ");
+};
 
 var countToNumber = function(inputNumValue) {
   var countArray = [];
   for (i = 0; i <= inputNumValue; i++) {
-    countArray.push(i);
+    countArray.push(i.toString());
   }
-  return countArray.join(", ");
+  return countArray;
 };
 
 var hal9000 = function(_numberInput) {
   var inputArray = _numberInput.split("");
-  if (!numTest.test(_numberInput)) {
-    return errorNaN
+  if (!numRegex.test(_numberInput)) {
+    return errorNaN;
   } else {
-    return countToNumber(parseInt(_numberInput));
-  }
+    var outputArray = countToNumber(parseInt(_numberInput));
+    return outputStringify(outputArray);
+  };
 };
 
 $(function() {
