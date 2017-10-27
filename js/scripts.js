ff52fc2d42a562bc879f0de990d3204d6a0f7359
Adam Smith <alspdx@gmail.com>
2017-10-27 10:23:44 -0700

    add function to count up to specified number and print as a string to page

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,27 @@
+var numTest = /^[0-9]*$/
+var errorNaN = "I think you know what the problem is just as well as I do. This mission is too important for me to allow you to jeopardize it. Please enter a numerical value, Dave."
+
+var countToNumber = function(inputNumValue) {
+  var countArray = [];
+  for (i = 0; i <= inputNumValue; i++) {
+    countArray.push(i);
+  }
+  return countArray.join(", ");
+};
+
 var hal9000 = function(_numberInput) {
-  if (isNaN(_numberInput)) {
-    return "I think you know what the problem is just as well as I do. This mission is too important for me to allow you to jeopardize it. Please enter a numerical value, Dave."
+  var inputArray = _numberInput.split("");
+  if (!numTest.test(_numberInput)) {
+    return errorNaN
   } else {
-    return ""
-  };
+    return countToNumber(parseInt(_numberInput));
+  }
 };
 
 $(function() {
   $("#user-input").submit(function(event) {
     event.preventDefault();
-    var numberInput = parseInt($("#number-input").val());
+    var numberInput = $("#number-input").val();
     var outputMessage = hal9000(numberInput);
     $("#output-message").text(outputMessage);
   });
