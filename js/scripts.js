5607b8673234db09acb2fd9da2bd39844f34360a
Adam Smith <alspdx@gmail.com>
2017-10-27 11:07:46 -0700

    add function to replace array items containing '1' with 'boop'

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,14 @@
-var zeroRegex = /[0]/
-var numRegex = /^[0-9]*$/
-var errorNaN = "I think you know what the problem is just as well as I do. This mission is too important for me to allow you to jeopardize it. Please enter a numerical value, Dave."
+var zeroRegex = /[0]/;
+    oneRegex = /[1]/;
+    zeroOneRegex = /[01]/;
+    numRegex = /^[0-9]*$/;
+    errorNaN = "I think you know what the problem is just as well as I do. This mission is too important for me to allow you to jeopardize it. Please enter a numerical value, Dave."
 
-var beepZeros = function(number) {
-  if (!zeroRegex.test(number)) {
+var beepBoop = function(number) {
+  if (!zeroOneRegex.test(number)) {
     return number;
+  } else if (!zeroRegex.test(number)) {
+    return "Boop"
   } else {
     return "Beep";
   };
@@ -12,7 +16,7 @@ var beepZeros = function(number) {
 
 var outputStringify = function(numArray) {
   var outputString = numArray.map(function(number) {
-    return beepZeros(number);
+    return beepBoop(number);
   });
   return outputString.join(", ");
 };
@@ -30,8 +34,7 @@ var hal9000 = function(_numberInput) {
   if (!numRegex.test(_numberInput)) {
     return errorNaN;
   } else {
-    var outputArray = countToNumber(parseInt(_numberInput));
-    return outputStringify(outputArray);
+    return outputStringify(countToNumber(parseInt(_numberInput)));
   };
 };
 
